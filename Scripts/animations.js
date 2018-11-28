598bb77af8ce5881cd4c38aed7a17ef9ce14ec3e
Sarah Waldie <sarah@sassafrasbiz.com>
2018-11-28 14:01:42 -0500

    Updates to homepage current items and some minor css tweaks.
    changed shape of headshot, made font bigger.

--- Scripts/animations.js
+++ Scripts/animations.js
@@ -1,78 +1,70 @@
 //typewriter effect on marquee.
 var i = 0;
-var txt = 'Web development with a smile! :)'; /* The text */
+var txt = "Web development with a smile! :)"; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 (function typeWriter() {
-    if (i < txt.length) {
-        document.getElementById("headline-text").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
+  if (i < txt.length) {
+    document.getElementById("headline-text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 })();
 
 // document.body.onload = typeWriter;
 
+$(window).scroll(function() {
+  //activate section header animations when the div is in the middle of the viewing window.
 
+  var h = window.innerHeight;
+  var top1 = h + h - 100;
+  var top1remove = h + h / 4;
+  var top2 = h + h + h + h - 300;
+  var top2remove = h + h + h;
 
+  if ($(this).scrollTop() > top1) {
+    $(".overlay1").addClass("scale-and-skew");
+  }
 
-$(window).scroll(function () {
+  if ($(this).scrollTop() < top1remove) {
+    $(".overlay1").removeClass("scale-and-skew");
+  }
 
-    //activate section header animations when the div is in the middle of the viewing window.
+  if ($(this).scrollTop() > top2) {
+    $(".overlay1").removeClass("scale-and-skew");
+    $(".overlay2").addClass("scale-and-skew");
+  }
 
-    var h = window.innerHeight;
-    var top1 = h + h - 100;
-    var top1remove = h + (h / 4);
-    var top2 = h + h + h + h - 300;
-    var top2remove = h + h + h;
+  if ($(this).scrollTop() < top2remove) {
+    $(".overlay2").removeClass("scale-and-skew");
+  }
 
-    if ($(this).scrollTop() > top1) {
-        $('.overlay1').addClass("scale-and-skew");
-    }
+  //activate sticky nav when static nav moves off the top of the scroll area and stick it to the top.
 
-    if ($(this).scrollTop() < top1remove) {
-        $('.overlay1').removeClass("scale-and-skew");
-    }
+  if ($(this).scrollTop() > 159) {
+    $(".sticky-nav").addClass("show-sticky-nav flex-div");
+  }
 
-    if ($(this).scrollTop() > top2) {
-        $('.overlay1').removeClass("scale-and-skew");
-        $('.overlay2').addClass("scale-and-skew");
-    }
-
-    if ($(this).scrollTop() < top2remove) {
-        $('.overlay2').removeClass("scale-and-skew");
-    }
-
-    //activate sticky nav when static nav moves off the top of the scroll area and stick it to the top. 
-
-    if ($(this).scrollTop() > 159) {
-        $('.sticky-nav').addClass("show-sticky-nav flex-div")
-    }
-
-    if ($(this).scrollTop() < 159) {
-        $('.sticky-nav').removeClass("show-sticky-nav flex-div")
-    }
-
+  if ($(this).scrollTop() < 159) {
+    $(".sticky-nav").removeClass("show-sticky-nav flex-div");
+  }
 });
 
 //Smooth scroll instead of jumping to anchor links.
 
-var $root = $('html, body');
+var $root = $("html, body");
 
-$('a[href^="#"]').click(function () {
-    $root.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 600);
+$('a[href^="#"]').click(function() {
+  $root.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    600
+  );
 
-    return false;
+  return false;
 });
 
-
-
-
-
-
-$(window).resize(function () { location.reload(); });
-
-
-
+// $(window).resize(function() {
+//   location.reload();
+// });
